2f03ebdf812d351ca93a2b8f1b7ab18c6670db10
JMBalanzar <45176438+SrGitQ@users.noreply.github.com>
2023-01-13 14:16:28 +0000

    basic structure

---
+++ src/analyzer/geojson.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class GeoJson:
+    """
+        Class to generate the needed data required by clients the raw data will
+        be analyzed and transformed to be used in the client side.
+
+        # Parameters
+
+        # Output
+
+        # Use
+    """
+    var_:str
+    
---
+++ src/analyzer/handler.py
@@ -0,0 +1,17 @@
+
+
+class Handler:
+    """
+        The class will maintain the pipelines activated and the crawler running once
+        this is started, also will generate the geojson, and it will update
+        the last geojson
+    """
+    def __init__(self, ):
+        #order the pipes
+        #run the routine with the pipes
+        #update
+        pass
+
+
+    def update(self, ):
+        pass
