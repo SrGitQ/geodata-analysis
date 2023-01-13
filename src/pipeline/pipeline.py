ba3d5f5ae01536e5dafe3ba5d86b0e6e4030fc20
JMBalanzar <45176438+SrGitQ@users.noreply.github.com>
2023-01-13 15:13:50 +0000

    pipeline class structure, and data folder

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,3 +1,37 @@
+from dataclasses import dataclass
 
+
+@dataclass
 class Pipeline:
-    pass
+    """
+        This class will execute all the basic methods of a pipeline
+        - load data
+        - preprocess data
+        - prepare data
+        - analysis
+        this methods can be rewriten for several uses
+    """
+    result:any = None
+
+    def __load_data__(self):
+        pass
+
+    def __preprocessing__(self):
+        pass
+
+    def __prepare__(self):
+        pass
+
+    def __anaylsis__(self):
+        pass
+
+    def run(self):
+        """
+            Will run all the methods of the pipeline and return the result
+        """
+        self.__load_data__()
+        self.__preprocessing__()
+        self.__prepare__()
+        self.__anaylsis__()
+
+        return self. result
