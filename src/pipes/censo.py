7c87efdff03462256556149372706cd8c260610e
JMBalanzar <1909011@upy.edu.mx>
2023-01-31 16:07:38 -0600

    censo processing

--- src/pipes/censo.py
+++ src/pipes/censo.py
@@ -2,13 +2,19 @@ from src.utils import Pipeline
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from src.utils.cords import dms2dd
 
 
 class Censo(Pipeline):
     """
     """
     def __preprocessing__(self):
-        pass
+        self.geodata = gpd.read_file('data/ITER_NALCSV20')#Change
+        self.geodata = self.geodata.MUN.str.cat(self.geodata.LOC)
+        self.geodata = self.geodata[['NOM_LOC', 'POBTOT', 'geometry', 'CVE_MUN']]
 
     def __anaylsis__(self):
-        pass
+        self.geodata['LATITUD'] = self.geodata['LATITUD'].apply(dms2dd)
+        self.geodata['LONGITUD'] = self.geodata['LONGITUD'].apply(dms2dd)
+        self.geodata = gpd.GeoDataFrame(self.geodata, geometry=gpd.points_from_xy(self.geodata.LONGITUD, self.geodata.LATITUD))
+
