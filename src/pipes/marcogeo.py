f40a5d63ffca505dfaf4eb35afcafc877baca905
JMBalanzar <1909011@upy.edu.mx>
2023-02-02 14:35:23 -0600

    marcogeo prepare rm files deleted

--- src/pipes/marcogeo.py
+++ src/pipes/marcogeo.py
@@ -10,8 +10,6 @@ class Marcogeo(Pipeline):
         route_inside = 'data/'+self.name
         unzip_file(route_inside, route_inside.replace('.zip', ''))
         rm_file(self.name, 'data')
-        rm_file('catologos', 'data/mg_2020_integrado')
-        rm_file('metadatos', 'data/mg_2020_integrado')
 
     def __preprocessing__(self):
         pass
