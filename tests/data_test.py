38a44ef4391e580d055268e3e337f911e4827a65
JMBalanzar <1909011@upy.edu.mx>
2023-04-14 17:25:33 -0600

    [TEST] raw files

--- tests/data_test.py
+++ tests/data_test.py
@@ -5,15 +5,15 @@ import os
 class RawDataTest(unittest.TestCase):
 
     def test_censo_1990_txt(self):
-        file = 'ITER_NALTXT90.txt'
+        file = 'ITER_NALTXT90.csv'
         self.assertTrue(file in os.listdir('data'), f'the file {file} is not at /data')
     
     def test_censo_2000_txt(self):
-        file = 'ITER_NALTXT00.txt'
+        file = 'ITER_NALCSV00.csv'
         self.assertTrue(file in os.listdir('data'), f'the file {file} is not at /data')
     
     def test_censo_2010_dbf(self):
-        file = 'ITER_NALDBF10.dbf'
+        file = 'ITER_NALCSV10.csv'
         self.assertTrue(file in os.listdir('data'), f'the file {file} is not at /data')
     
     def test_censo_2020_csv(self):
@@ -25,35 +25,12 @@ class RawDataTest(unittest.TestCase):
         self.assertTrue(file in os.listdir('data'), f'the file {file} is not at /data')
     
     def test_mg_2020_integrado(self):
-        files = [
-            '00a.cpg',
-            '00a.dbf',
-            '00a.prj',
-            '00a.shp',
-            '00a.shx',
-            '00ent.cpg',
-            '00ent.dbf',
-            '00ent.prj',
-            '00ent.shp',
-            '00ent.shx',
-            '00l.cpg',
-            '00l.dbf',
-            '00l.prj',
-            '00l.shp',
-            '00l.shx',
-            '00lpr.cpg',
-            '00lpr.dbf',
-            '00lpr.prj',
-            '00lpr.shp',
-            '00lpr.shx',
-            '00mun.cpg',
-            '00mun.dbf',
-            '00mun.prj',
-            '00mun.shp',
-            '00mun.shx',
-        ]
-        for file in files:
-            self.assertTrue(file in os.listdir('data/MG_2020_Integrado/conjunto_de_datos'), f'the file {file} is not at /data')
+        file = '00mun.shp'
+        self.assertTrue(file in os.listdir('data'), f'the file {file} is not at /data')
+
+    def test_files_unwanted(self):
+        wanted_files = ['ITER_NALTXT90.csv', 'ITER_NALCSV00.csv', 'ITER_NALCSV10.csv', 'ITER_NALCSV20.csv', 'Base_SUN_2018.csv', '00mun.shp', '00mun.shx']
+        self.assertTrue(all([file in wanted_files for file in os.listdir('data')]), f'There are some files that are unwanted in the list: \n{wanted_files}\nfiles in the folder:\n{os.listdir("data")}')
 
 
 if __name__ == "__main__":
